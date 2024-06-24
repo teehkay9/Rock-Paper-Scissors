6fa9c9f3822d914aa4adc03f9567a5445271cf0b
Thomas <thomashoelsken@yahoo.de>
2024-06-24 07:46:24 +0200

    Update rps.js with playRound function

--- rps.js
+++ rps.js
@@ -11,3 +11,22 @@ function getHumanChoice() {
   const humanChoice = Number(prompt("For 'Rock' enter 0, for 'Paper' enter 1 and for 'Scissor' enter 2: "));
   return choices[humanChoice];
 }
+
+function playRound(humanChoice, computerChoice) {
+  console.log(`Round: ${round}. User score = ${humanScore}, Computer score = ${computerScore}`);
+  console.log(`User chooses ${humanChoice}, Computer chooses ${computerChoice}.`);
+  if (humanChoice.toLowerCase() === computerChoice.toLowerCase()) {
+    console.log("It's a draw!");
+  } else if (
+    (humanChoice.toLowerCase() === "rock" && computerChoice.toLowerCase() === "scissor") ||
+    (humanChoice.toLowerCase() === "paper" && computerChoice.toLowerCase() === "rock") ||
+    (humanChoice.toLowerCase() === "scissor" && computerChoice.toLowerCase() === "paper")
+  ) {
+    console.log("User won!");
+    humanScore += 1;
+  } else {
+    console.log("Computer won!");
+    computerScore += 1;
+  }
+  round += 1;
+}
