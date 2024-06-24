90f589b58ba64aa6c25104c01a079ef0a51af1ae
Thomas <thomashoelsken@yahoo.de>
2024-06-24 07:43:49 +0200

    Update rps.js with getHumanChoice function
    - added the function getHumanChoice to rps.js

--- rps.js
+++ rps.js
@@ -5,3 +5,9 @@ function getComputerChoice() {
   const random_choice = Math.floor(Math.random() * 3);
   return choices[random_choice];
 }
+
+function getHumanChoice() {
+  const choices = ["Rock", "Paper", "Scissor"];
+  const humanChoice = Number(prompt("For 'Rock' enter 0, for 'Paper' enter 1 and for 'Scissor' enter 2: "));
+  return choices[humanChoice];
+}
