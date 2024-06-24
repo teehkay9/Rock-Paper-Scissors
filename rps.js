06b6a067b5ae4bcbc95e1dfadb7bbae9e9f622fb
Thomas <thomashoelsken@yahoo.de>
2024-06-24 07:40:39 +0200

    Update rps.js with function getComputerChoice
    - added the function getComputerChoice to rps.js

--- rps.js
+++ rps.js
@@ -1,1 +1,7 @@
 console.log("Welcome to Rock Paper Scissors!");
+
+function getComputerChoice() {
+  const choices = ["Rock", "Paper", "Scissor"];
+  const random_choice = Math.floor(Math.random() * 3);
+  return choices[random_choice];
+}
